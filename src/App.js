5964d363d27b50861b61342f1408252f26279503
iamkunal9 <ks4428560@gmail.com>
2023-02-22 08:36:13 +0530

    update

--- src/App.js
+++ src/App.js
@@ -3,17 +3,27 @@ import './App.css';
 // var islogin = false;
 // var data;
 function App() {
-  function handleClick(){
-    window.open("https://kunalsharma0553.pythonanywhere.com/login/google");
+  function handleClick(clg){
+    if(clg==='piet'){
+      document.getElementById("info").innerHTML = "Registration For PIET will open soon";
+    // window.open("https://kunalsharma0553.pythonanywhere.com/login/google");
+    }
+    else{
+      window.open("https://www.aarohanpce.com/");
+      }
   }
 
   return (
     <div className="App">
       <div class="container">
   <div class="center">
-  <button type="button" class="login-with-google-btn" onClick={handleClick}>
-  Sign in with Google
-</button>
+    <p id="info">Click on respective collage</p>
+  <div class="left-btn">
+  <button class="glow-on-hover" type="button" onClick={() => handleClick('piet')}>PIET</button>
+</div>
+<div class="right-btn">
+<button class="glow-on-hover" type="button" onClick={() => handleClick('ppp')}>PCE</button>
+</div>
   </div>
 </div>
 
